29a53eb023ed5b7bb80d1b61679ea1c548dc6686
hicha <hichamakil2018@gmail.com>
2026-01-15 20:54:54 +0100

    Initial commit

---
+++ src/test/java/org/example/backend_med/BackendMedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backend_med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
